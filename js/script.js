851085494b75b8cb5dbf15741b3965eb3dbb5b01
Полина <polinapashko859.com>
2026-02-16 23:43:24 +0300

    Начало проекта MovieHub: базовая структура и стили

---
+++ js/script.js
@@ -0,0 +1,31 @@
+console.log("MovieHub: Система готова.");
+
+const playBtn = document.getElementById('welcomeBtn');
+
+playBtn.addEventListener('click', () => {
+    // Создаем эффект "Плеера" как в твоем Desktop-2
+    const playerOverlay = document.createElement('div');
+    playerOverlay.style.position = 'fixed';
+    playerOverlay.style.top = '0';
+    playerOverlay.style.left = '0';
+    playerOverlay.style.width = '100%';
+    playerOverlay.style.height = '100%';
+    playerOverlay.style.backgroundColor = 'black';
+    playerOverlay.style.zIndex = '2000';
+    playerOverlay.style.display = 'flex';
+    playerOverlay.style.justifyContent = 'center';
+    playerOverlay.style.alignItems = 'center';
+    playerOverlay.style.cursor = 'pointer';
+
+    playerOverlay.innerHTML = `
+        <div style="width: 0; height: 0; border-top: 50px solid transparent; border-left: 80px solid white; border-bottom: 50px solid transparent;"></div>
+        <p style="position: absolute; bottom: 20px; color: gray;">Нажмите, чтобы закрыть плеер</p>
+    `;
+
+    document.body.appendChild(playerOverlay);
+
+    // Закрыть плеер при клике
+    playerOverlay.onclick = () => {
+        playerOverlay.remove();
+    };
+});
